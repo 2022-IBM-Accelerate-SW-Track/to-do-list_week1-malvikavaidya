559c6c0bbf0cd61b2f03b781bff2096ba1158a50
Malvika Vaidya <malvikavaidya@malvikas-mbp.lan>
2022-06-09 15:37:44 -0500

    adding todo list

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import './About.css';
+import headshot from "../assets/headshot.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+          <div class="split left">
+          <div className="centered">
+          <img 
+            className="profile_image"
+            src ={headshot}
+          ></img>
+          </div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Malvika Vaidya</div>
+        <div className="brief_description">
+          I like reading, working out, and eating good food!
+        </div>
+      </div>
+    </div>
+  </div>
+        
+        
       </div>
     )
   }
